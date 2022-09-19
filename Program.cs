b414614e1ca0685a352d33c02831233637b8e66d
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-09-19 03:38:34 +0300

    задача решена

--- Program.cs
+++ Program.cs
@@ -1,45 +1,57 @@
 ﻿Console.Clear();
 
-string[] arrayA = new string[10];
-string a = "";
+string[] arrayA =
+ {
+    "ad#", 
+    "b1",
+    "5cccc",
+    "Roar",
+    "end"
+};
 
-Console.WriteLine("Введите значения элементов массива строк (до десяти элементов).");
-FillArray(arrayA);
-
-string[] FillArray(string[] arr) // метод задания значений
-{
-    for (int i = 0; i < arrayA.Length; i++)
-    {
-        Console.WriteLine($"Введите значение {i + 1} элемента. Для досрочного завершения заполнения введите STOP");
-        a = Console.ReadLine()!;
-        if (a == "STOP")
-        {
-            break;
-        }
-        arrayA[i] = a;
-    }
-    return arr;
-}
-
-Console.WriteLine("Вы ввели следующий массив");
+Console.Write("Исходный массив: ");
 PrintArray(arrayA);
+
+Console.Write("\nИз изначально введенного массива массив с длиной строк меньше трех символов:");
+
+string[] arrayB = LessThanThreeArray(arrayA);
+PrintArray(arrayB);
 
 void PrintArray (string[] arr) // метод распечатки массивов без вывода пустых значений
 {
     for (int i = 0; i < arr.Length; i++)
     {
         {
-            if (arr[i] == null)
+            if (arr[i] == null && i == 0) //проверка на первый пустой элемент
             {
-                Console.Write("]");
+                Console.Write("[]"); 
                 break;
             }
-            else if (i == 0) { Console.Write($"[{arrayA[i]}"); }
-            else if (i == arrayA.Length - 1) { Console.Write($"{arrayA[i]}]"); }
-            else if (i != arrayA.Length - 1) { Console.Write($", {arrayA[i]}"); }
+            if (arr[i] == null)
+            {
+                Console.Write("]"); //проверка на пустой элемент для закрытия массива
+                break;
+            }
+            else if (i == 0) { Console.Write($"[{arr[i]}"); } //печатаем красиво первый символ массива
+            else if (i == arr.Length - 1) { Console.Write($"{arr[i]}]"); } //печатаем красиво последний символ массива
+            else if (i != arr.Length - 1) { Console.Write($", {arr[i]}"); } //печатаем значения между первым и последним
         }
     }
 }
 
-Console.WriteLine("Вы ввели следующий массив");
-PrintArray(arrayA);
+string[] LessThanThreeArray (string[] arr) // метод распечатки подходящих значений массива меньше трех символов от исходного массива
+{
+    int i = 0;
+    string[] result = new string [arr.Length];
+    
+    
+    foreach (var item in arr)
+    {
+            if (item.Length <= 3)
+            {
+                result[i] = item;
+                i++;
+            }
+    }
+    return result;
+}
